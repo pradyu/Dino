17f498de05906b7b456b26adf8b63dca974cb4e0
Abhijit Nukalapati <abhijit@brandingbrand.com>
2014-03-17 23:07:57 -0400

    removing backend files to seperate andorid app implementation

--- src/main/java/hello/GreetingRepository.java
+++
@@ -1,9 +0,0 @@
-package hello;
-
-import java.util.List;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface GreetingRepository extends MongoRepository<Greeting, String> {
-	public List<Greeting> findByContent(String content);
-}
